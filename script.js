b6e7dc374e4b9226b3130488f30619aaee1e5627
Eli Holzman <eli770440@gmail.com>
2024-02-02 13:11:28 +0200

    adding the simon game files to repository

---
+++ script.js
@@ -0,0 +1,314 @@
+let arrRound = []; // Array round
+let gameCounter; // Game counter
+let userCounter; // User counter
+
+// Colors
+const green = document.getElementById('green');
+const red = document.getElementById('red');
+const blue = document.getElementById('blue');
+const yellow = document.getElementById('yellow');
+
+let userInputArr = [];
+
+let gameTurn;
+
+const startBtn = document.getElementById('btnLoad');
+const output = document.getElementById('output');
+
+
+// Function (A) אתחול ואיפוס
+// This function does NOT start the game, you need to press a button to start the game
+function initGame() {
+    userInputArr = [];
+    userCounter = 0; // איפוס משתנים
+    arrRound = []; // איפוס המערך
+    console.log('Game initiated...');
+}
+
+
+// Function (B) ניהול שלב/סיבוב
+function round() {
+    output.innerHTML = 'Good luck!';
+
+    // 1. Create step and add as a last elemnt to the array
+    // arrRound[gameCounter];
+    userInputArr = [];
+    gameTurn = true;
+
+    // :מהסיבוב השני ומעלה
+    // בתור של המחשב - עובר על כל הלחיצות הקודמות לפני הלחיצה החדשה
+
+    for (let i = 1; i <= arrRound.length; i++) {
+        setTimeout(() => {
+            if (gameTurn == true) mySwitch(arrRound[i - 1]);
+        }, i * 1000);
+
+    }
+
+    // הלחיצה החדשה של המחשב
+    setTimeout(() => {
+        let newStep = createStep(); // function (C) --> Random num
+        arrRound.push(newStep);
+        gameTurn = false;
+        // עובר לתור המשתמש
+        userTurn(arrRound); // Function (D) 
+    }, 1000 + (arrRound.length * 1000));
+
+}
+
+
+// Function (C)
+// 1. Creats a random number from 1 to 4
+// 2. Returns the result and/or the color 
+function createStep() {
+    let random = Math.round(Math.floor((Math.random() * 4) + 1));
+    mySwitch(random);
+    return random;
+}
+
+function mySwitch(num) {
+    document.querySelectorAll('.colorBtn').forEach(button => button.style.cursor = 'progress');
+
+    switch (num) {
+        case 1:
+            green.click();
+            green.style.borderColor = '#fff';
+            green.style.borderWidth = '5px';
+            setTimeout(() => { green.style.borderColor = '#000'; }, 500);
+
+            console.log('green game clicked');
+            break;
+
+        case 2:
+            red.click();
+            red.style.borderColor = '#fff';
+            red.style.borderWidth = '5px';
+            setTimeout(() => { red.style.borderColor = '#000'; }, 500);
+
+            console.log('red game clicked');
+
+            break;
+
+        case 3:
+            blue.click();
+            blue.style.borderColor = '#fff';
+            blue.style.borderWidth = '5px';
+
+            setTimeout(() => { blue.style.borderColor = '#000' }, 500);
+
+
+            console.log('blue game clicked');
+
+            break;
+
+        case 4:
+            yellow.click();
+            yellow.style.borderColor = '#fff';
+            yellow.style.borderWidth = '5px';
+
+            setTimeout(() => { yellow.style.borderColor = '#000' }, 500);
+
+
+            console.log('yellow game clicked');
+
+            break;
+    }
+}
+
+
+
+// Function (D)
+function userTurn(arrRound) {
+    // Get input from the user
+    // const userInput = prompt('Please enter your guess');
+    let a = 0;
+    document.querySelectorAll('.colorBtn').forEach(button => button.style.cursor = 'pointer');
+
+    // Create a function to handle button clicks
+    function handleButtonClick(event) {
+
+        const clickedBtn = event.target;
+        console.log(clickedBtn.id);
+        if (gameTurn == false) {
+
+            switch (clickedBtn.id) {
+                case 'green':
+                    userInputArr.push(1);
+                    green.style.borderColor = '#fff';
+                    green.style.borderWidth = '5px';
+
+                    setTimeout(() => { green.style.borderColor = '#000' }, 500);
+                    break;
+
+                case 'red':
+                    userInputArr.push(2);
+                    red.style.borderColor = '#fff';
+                    red.style.borderWidth = '5px';
+
+                    setTimeout(() => { red.style.borderColor = '#000' }, 500);
+
+                    break;
+
+                case 'blue':
+                    userInputArr.push(3);
+                    blue.style.borderColor = '#fff';
+                    blue.style.borderWidth = '5px';
+
+                    setTimeout(() => { blue.style.borderColor = '#000' }, 500);
+                    break;
+
+                case 'yellow':
+                    userInputArr.push(4);
+                    yellow.style.borderColor = '#fff';
+                    yellow.style.borderWidth = '5px';
+
+                    setTimeout(() => { yellow.style.borderColor = '#000' }, 500);
+                    break;
+            }
+        }
+        a++;
+        // Remove the event listener after a button is clicked
+        if (a === arrRound.length) {
+            document.querySelectorAll('.colorBtn').forEach(button => {
+                button.removeEventListener('click', handleButtonClick);
+            });
+            console.log('remove');
+            endTurn(userInputArr, arrRound);
+
+        }
+    }
+
+
+    document.querySelectorAll('.colorBtn').forEach(button => {
+        button.addEventListener('click', handleButtonClick);
+    });
+
+}
+
+
+function endTurn(userInputArr, arrRound) {
+    // Continue with the game logic (compare input with the array, etc.)
+    if (arraysAreEqual(userInputArr, arrRound)) {
+        startBtn.value = 'Success!';
+
+        setTimeout(() => {
+            startBtn.value = 'Start';
+            userCounter++;
+            round();
+        }, 1000);
+
+
+    } else {
+        output.innerHTML = `Game over! Your score: ${userCounter}. <br/> Click start to play again.`;
+        initGame();
+    }
+}
+
+function arraysAreEqual(arr1, arr2) {
+    return JSON.stringify(arr1) === JSON.stringify(arr2);
+}
+
+
+
+initGame();
+document.getElementById('btnLoad').addEventListener('click', round);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Function (D)
+// function userTurn() {
+//     // Get input from the user
+//     // const userInput = prompt('Please enter your guess');
+//     let userInput
+//     document.querySelectorAll('.colorBtn').forEach(button => {
+//         button.addEventListener('click', (event) => {
+//             const clicedBtn = event.target;
+//             switch (clicedBtn) {
+//                 case green:
+//                     userInput = 1;
+//                     break;
+
+//                 case red:
+//                     userInput = 2;
+//                     break;
+
+//                 case blue:
+//                     userInput = 3;
+//                     break;
+
+//                 case yellow:
+//                     userInput = 4;
+//                     break;
+//             }
+//         })
+//     });
+
+
+//     // Compar input with the array
+//     if (userInput === arrRound[gameCounter]) {
+//         console.log('Success!');;
+//         gameCounter++;
+//         userCounter++;
+//         round();
+//     } else {
+//         console.log(`Game over! Your score: ${userCounter}`);
+//         initGame();
+//     }
+// }
+
+
+
+
+
+// arrRound.push(random);
+
+// for (let i = 0; i < arrRound.length; i++) {
+//     setTimeout(() => {
+//         switch (arrRound[i]) {
+//             case 1:
+//                 green.click();
+//                 green.style.backgroundColor = 'lightgreen';
+//                 console.log('green clicked');
+//                 break;
+
+//             case 2:
+//                 red.click();
+//                 red.style.backgroundColor = 'crimson';
+
+//                 console.log('red clicked');
+
+//                 break;
+
+//             case 3:
+//                 blue.click();
+//                 blue.style.backgroundColor = 'lightblue';
+
+//                 console.log('blue clicked');
+
+//                 break;
+
+//             case 4:
+//                 yellow.click();
+//                 yellow.style.backgroundColor = 'lightyellow';
+
+//                 console.log('yellow clicked');
+
+//                 break;
+//         }
+//     }, 1000 * (i + 1));
+// }
